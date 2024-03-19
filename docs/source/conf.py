febbe98ea10268c8d9997687a24b2ece7e1691cc
Lylist <1303404574@qq.com>
2024-03-19 17:33:46 +0800

    docs: add marddown in readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import sphinx_rtd_theme
+from recommonmark.parser import CommonMarkParser
 
 project = 'snngrow'
 copyright = '2024, utarn'
